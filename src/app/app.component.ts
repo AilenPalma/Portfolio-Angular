07ebc5cb7a1b60957214564de1e66e2dbfe08f5f
Ailen PALMA <134826493+AilenPalma@users.noreply.github.com>
2024-12-09 10:46:02 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePagesComponent } from "./pages/home-pages/home-pages.component";
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ContactIconComponent } from '../assets/contact-icon/contact-icon.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [CommonModule, RouterOutlet,NavbarComponent,ContactIconComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio-angular';
+  title = 'Soy export appcomponentts';
 }
