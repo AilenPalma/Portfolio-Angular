15f7a86126afefb48399740d3a997e3ca434a85c
Ailen PALMA <134826493+AilenPalma@users.noreply.github.com>
2024-12-09 10:15:33 -0300

    Add files via upload

---
+++ src/app/features/components/projects/projects.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-projects',
+  imports: [RouterModule],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.css'
+})
+export class ProjectsComponent {
+
+}
